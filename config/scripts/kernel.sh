0876717c566caae5fb3537fe7cdb9fd1cb58d86a
Luiza <114603943+luizansounds@users.noreply.github.com>
2024-06-06 19:54:29 -0300

    Update and rename kernel.nu to kernel.sh

--- config/scripts/kernel.sh
+++ config/scripts/kernel.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S nu
+#!/usr/bin/env sh
 use lib/std.nu [get_fedora_version, fetch_copr]
 let FEDORA_MAJOR_VERSION = (get_fedora_version)
 let COPR_FILE = (fetch_copr  $"https://copr.fedorainfracloud.org/coprs/whitehara/kernel-tkg/repo/fedora-($FEDORA_MAJOR_VERSION)/whitehara-kernel-tkg-fedora-($FEDORA_MAJOR_VERSION).repo")

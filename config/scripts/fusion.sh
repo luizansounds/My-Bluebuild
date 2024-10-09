b2b9875f26be19d1d884f01c4804b67f32f8980c
Luiza <114603943+luizansounds@users.noreply.github.com>
2024-10-09 19:00:21 -0300

    Create fusion.sh

---
+++ config/scripts/fusion.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
